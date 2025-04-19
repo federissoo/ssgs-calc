deccd94cb0321e0e8ae4de4469aefc98af384a23
fderissoo <fede.risso2002@gmail.com>
2025-04-19 15:53:48 +0200

    Correzione test

--- math.test.js
+++ math.test.js
@@ -1,23 +1,13 @@
-const { sum, sub, mul, div } = require('./math');
+const { sum } = require('./math');
 
-describe('Funzioni matematiche', () => {
-  test('sum: somma 2 + 3 = 5', () => {
-    expect(sum(2, 3)).toBe(5);
+describe('Sum Tests', () => {
+  test('sum 2 + 2', () => {
+    const risultato = sum(2, 2);
+    expect(risultato).toBe(4);
   });
 
-  test('sub: 5 - 2 = 3', () => {
-    expect(sub(5, 2)).toBe(3);
-  });
-
-  test('mul: 4 * 3 = 12', () => {
-    expect(mul(4, 3)).toBe(12);
-  });
-
-  test('div: 10 / 2 = 5', () => {
-    expect(div(10, 2)).toBe(5);
-  });
-
-  test('div by zero: dovrebbe lanciare un errore', () => {
-    expect(() => div(10, 0)).toThrow("Cannot divide by zero");
+  test('sum 3 + 5', () => {
+    const risultato = sum(3, 5);
+    expect(risultato).toBe(7);
   });
 });
